305ba07c48c3523e1087740149dbc33a0fe0f2d5
Roberto Ribeiro <robertoribeiro.code@gmail.com>
2024-04-05 14:38:26 -0300

    solution exercise 84

--- 88. Merge Sorted Array/merge_sorted_array.js
+++ 88. Merge Sorted Array/merge_sorted_array.js
@@ -6,26 +6,11 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  const lenght = m + n
-
-  for (let i = 0; i < lenght; i++) {
-    if (nums1[i] == 0 && m == n) {
-      nums1[i] = nums2[i - n]
-    } else if (nums1[i] == 0 && m > n) {
-      nums1[i] = nums2[i - (m + n - 1)];
-    } else if (nums1[i] == 0 && m < n && m != 0) {
-      nums1[i] = nums2[i - 1];
-    } else if (nums1[i] == 0) {
-      nums1[i] = nums2[i];
+  for (let i = 0; i < m + n; i++) {
+    if (nums1[i] == 0) {
+      nums1[i] = nums2[i - m] || 0;
     }
   }
 
-
-  return nums1.sort();
+  nums1.sort((a, b) => a - b);
 };
-
-
-// num1 [4,0,0,0,0,0] m = 1
-// num2 [1,2,3,5,6] n = 5
-
-// [2, 3, 4, 5, 6, undefined];
